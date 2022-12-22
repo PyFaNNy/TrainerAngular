6daac081290b964ea94bbf6d7afa29681820d62e
PyFaNNy <fanny.koob@gmail.com>
2022-12-22 23:12:44 +0300

    Init Trainer.Client

---
+++ Trainer.Frontend/Trainer/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {HeaderComponent} from "./Header/header.component";
+import {FooterComponent} from "./Footer/footer.component";
+import {BodyComponent} from "./Body/body.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
