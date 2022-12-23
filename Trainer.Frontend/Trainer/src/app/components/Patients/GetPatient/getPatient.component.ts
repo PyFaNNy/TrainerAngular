e153b87b9cdd54db54d00689a52932c35cd4beb3
PyFaNNy <fanny.koob@gmail.com>
2022-12-24 01:09:40 +0300

    add some new component

---
+++ Trainer.Frontend/Trainer/src/app/components/Patients/GetPatient/getPatient.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-getPatient',
+  templateUrl: './getPatient.component.html'
+})
+
+export class GetPatientComponent {
+
+}
---
+++ Trainer.Frontend/Trainer/src/app/components/Patients/GetPatients/getPatients.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-getPatients',
+  templateUrl: './getPatients.component.html'
+})
+
+export class GetPatientsComponent {
+
+}
