203747b9996c679d031987ece0188088b113df3d
PyFaNNy <fanny.koob@gmail.com>
2022-12-26 18:29:35 +0300

    Add new component

---
+++ Trainer.Frontend/Trainer/src/app/components/VerifyCode/verify-code.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './verify-code.component.html'
+})
+
+export class VerifyCodeComponent {
+
+}
