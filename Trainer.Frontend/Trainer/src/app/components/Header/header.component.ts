386487cb64290ce2d9dfd4314b6d2f4f9dfab0fb
PyFaNNy <fanny.koob@gmail.com>
2023-01-16 22:48:07 +0300

    Add some logic with auth state

--- Trainer.Frontend/Trainer/src/app/components/Header/header.component.ts
+++ Trainer.Frontend/Trainer/src/app/components/Header/header.component.ts
@@ -1,9 +1,29 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {TokenService} from "../../services/token.service";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  email: string = '';
+  IsAuth: boolean = false;
+  constructor(private tokenService: TokenService, private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    let token = this.tokenService.getToken();
+    this.IsAuth = token != null;
+  }
+
+  logout()
+  {
+    this.authService
+      .logout();
+    this.ngOnInit();
+    this.router.navigate(['/'])
+  }
 }
