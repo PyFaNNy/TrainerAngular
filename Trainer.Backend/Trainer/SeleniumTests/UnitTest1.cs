44a0c1c7a4c2c25e10e2a84107ce7e939297534b
PyFaNNy <fanny.koob@gmail.com>
2023-01-24 23:18:22 +0300

    First Auth test

--- Trainer.Backend/Trainer/SeleniumTests/UnitTest1.cs
+++ Trainer.Backend/Trainer/SeleniumTests/UnitTest1.cs
@@ -6,16 +6,40 @@ namespace SeleniumTests;
 public class Tests
 {
     private IWebDriver _driver;
+    
+    private readonly By _signInButton = By.XPath("//a[text()='Login']");
+    private readonly By _loginButton = By.XPath("//button[@id='loginBtn']");
+    private readonly By _loginInput = By.XPath("//input[@id='mat-input-0']");
+    private readonly By _passwordInput = By.XPath("//input[@id='mat-input-1']");
+    private readonly By _userEmail = By.XPath("//a[@id='userEmail']");
+    
+    private readonly string _userName = "trainerdoctor@gmail.com";
+    private readonly string _userPassword = "doctor";
+    
     [SetUp]
     public void Setup()
     {
         _driver = new ChromeDriver();
-        _driver.Navigate().GoToUrl("https://www.google.com/");
+        _driver.Navigate().GoToUrl("http://localhost:4200/home");
+        _driver.Manage().Window.Maximize();
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        var signIn = _driver.FindElement(_signInButton);
+        signIn.Click();
+        
+        var login = _driver.FindElement(_loginInput);
+        var password = _driver.FindElement(_passwordInput);
+        var loginBtn = _driver.FindElement(_loginButton);
+        login.SendKeys(_userName);
+        password.SendKeys(_userPassword);
+        
+        loginBtn.Click();
+        Thread.Sleep(1000);
+        var email = _driver.FindElement(_userEmail).Text;
+        
+        Assert.AreEqual(_userName, email);
     }
 }
