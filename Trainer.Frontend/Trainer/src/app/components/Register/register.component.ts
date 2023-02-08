910c5499b9fc36fc95add75c718a420fc7bfb1a0
PyFaNNy <fanny.koob@gmail.com>
2023-02-08 23:42:37 +0300

    Add reactive forms to Register

--- Trainer.Frontend/Trainer/src/app/components/Register/register.component.ts
+++ Trainer.Frontend/Trainer/src/app/components/Register/register.component.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import {User} from "../../models/user";
 import {UserService} from "../../services/user.service";
 import {OtpService} from "../../services/otp.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-addPatient',
@@ -11,24 +12,36 @@ import {OtpService} from "../../services/otp.service";
 
 export class RegisterComponent {
   showSpinner: boolean = false;
-  user: User =new User;
+  registerForm: FormGroup;
   errors: any;
 
   constructor(private userService: UserService, private otpService: OtpService, private router: Router)
   {
-    this.user.role = 'Doctor';
+    this._createForm();
   }
 
-  createUser() {
+  submit() {
     this.showSpinner= true;
     this.userService
-      .createUser(this.user)
+      .createUser(this.registerForm.value)
       .subscribe(value => {
-          this.router.navigate(['/verifycode',this.user.email,'Registration'])
+          this.router.navigate(['/verifycode',this.registerForm.value.email,'Registration'])
         },
         error => {
           this.showSpinner= false;
           this.errors = error.error.errors
         });
+  }
+
+  private _createForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null),
+      role: new FormControl(null),
+      lastName: new FormControl(null),
+      middleName: new FormControl(null),
+      firstName: new FormControl(null),
+      confirmPassword: new FormControl(null)
+    })
   }
 }
