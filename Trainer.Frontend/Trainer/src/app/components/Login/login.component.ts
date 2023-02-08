d64006d10cb910e724673487c1aa1f3fc49ed493
PyFaNNy <fanny.koob@gmail.com>
2023-02-08 23:33:54 +0300

    Add reactive forms to Login

--- Trainer.Frontend/Trainer/src/app/components/Login/login.component.ts
+++ Trainer.Frontend/Trainer/src/app/components/Login/login.component.ts
@@ -1,10 +1,10 @@
 import {Component} from "@angular/core";
-import {User} from "../../models/user";
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
 import {AuthService} from "../../services/auth.service";
 import {OtpService} from "../../services/otp.service";
 import {Otp} from "../../models/otp";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-addPatient',
@@ -13,16 +13,25 @@ import {Otp} from "../../models/otp";
 
 export class LoginComponent {
   showSpinner: boolean = false;
-  user: User = new User;
+  loginForm: FormGroup;
   errors: any;
 
   constructor(private authService: AuthService, private otpService: OtpService, private router: Router) {
+    this._createForm();
   }
 
-  login() {
+  private _createForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null)
+    })
+  }
+
+  submit()
+  {
     this.showSpinner = true;
     this.authService
-      .login(this.user)
+      .login(this.loginForm.value)
       .subscribe(value => {
           this.showSpinner = false;
           window.location.replace("");
@@ -38,9 +47,9 @@ export class LoginComponent {
 
   reset() {
     this.otpService
-      .resetPassowrdRequest(this.user.email)
+      .resetPassowrdRequest(this.loginForm.get('email')?.value)
       .subscribe(value => {
-          this.router.navigate(['/verifycode', this.user.email, 'ResetPassword'])
+          this.router.navigate(['/verifycode', this.loginForm.get('email')?.value, 'ResetPassword'])
         },
         error => {
           this.errors = error.error
